9469d7b4de823a2e8d565602a0304965d9e01452
shermhamel22 <shermhamel22@github.com>
2026-01-05 21:42:22 -0500

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/melbournelawnsite/',
   server: {
     host: "::",
     port: 8080,
